481ab18a4e1a0e25a957e29d9cb9f6b5edd988d4
Bheema Shanker Neyigapula <148355250+Bheema-Shanker-Neyigapula@users.noreply.github.com>
2023-12-30 19:38:12 +0530

    Create classification.py
    This example uses a classic dataset called the Iris dataset for a classification task. Note that this is a basic example, and in a real-world scenario, you might need more data preprocessing, feature engineering, and hyperparameter tuning for better performance. Additionally, you can explore other machine learning algorithms and datasets depending on your project requirements.

---
+++ classification.py
@@ -0,0 +1,32 @@
+# Import necessary libraries
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
+# Load the Iris dataset
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Standardize the features
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Create a k-nearest neighbors classifier
+knn_classifier = KNeighborsClassifier(n_neighbors=3)
+
+# Train the classifier
+knn_classifier.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = knn_classifier.predict(X_test)
+
+# Evaluate the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy * 100:.2f}%")
